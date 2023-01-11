1cb339483f2481296c20fdae3c15d5c270f8d644
sophiia220403 <stoesss@dhbw-loerrach.de>
2023-01-11 22:11:58 +0100

    .

--- Schaltjahr.py
+++ Schaltjahr.py
@@ -1,8 +1,10 @@
-eingabeJahr = input("Bitte Jahreszahl eingeben \n")
+eingabeJahr = int(input("Bitte Jahreszahl eingeben \n"))
 
-if eingabeJahr //4:
+if eingabeJahr //4 and not eingabeJahr //100:
     print("Schaltjahr")
 
-if eingabeJahr !=//100:
+if eingabeJahr //400:
+    print("Schaltjahr")
 
-
+if eingabeJahr //100 and not eingabeJahr//400:
+    print("kein Schaltjahr")
