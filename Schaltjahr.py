33c7ac1dbd211144810aca09cb3060dae6238753
sophiia220403 <stoesss@dhbw-loerrach.de>
2023-01-11 16:10:57 +0100

    11.01.23

---
+++ Schaltjahr.py
@@ -0,0 +1,8 @@
+eingabeJahr = input("Bitte Jahreszahl eingeben \n")
+
+if eingabeJahr //4:
+    print("Schaltjahr")
+
+if eingabeJahr !=//100:
+
+
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-vorname = 'Sophia'
-anzFahrraeder = 2
-print(f"{vorname} hat {anzFahrraeder} Fahrräder")
-x = 1
-y = 4
-print(y / x)
+import random
+
+vornamen = ['Axel', 'Elke', 'Martin']
+eingabeName = input("Bitte Namen eingeben \n")
+position = int(input("Bitte Position eingeben \n"))
+
+"""var 1
+if position > 2:
+    print("Bitte eine Zahl zwischen 0 und 2 eingeben")
+    position = int(input("Bitte Position eingeben \n"))
+
+else:
+    vornamen[position] = eingabeName
+    print(vornamen)"""
+
+"""var 2
+if position < len(vornamen): #and position >=0#
+    vornamen[position] = eingabeName
+    print(vornamen)
+
+else:
+    print("Bitte eine Zahl zwischen 0 und 2 eingeben")
+    position = int(input("Bitte Position eingeben \n"))"""
+
+#elif bei pos=0
