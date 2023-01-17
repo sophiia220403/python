347d3cdd216a7fab97f6ac03786315f8c42c4df3
sophiia220403 <stoesss@dhbw-loerrach.de>
2023-01-17 14:57:02 +0100

    Aufgabe Zuhause mit while Schleife

---
+++ whileSchleife.py
@@ -0,0 +1,15 @@
+# https://www.python-lernen.de/while-schleife.htm
+# https://www.python-lernen.de/uebung-zahlenraten.htm
+
+"""schleife in Epoche einfügen mit 'soll ich weiter machen?'
+   -> muss nicht mit Epoche sein, hauptsache input drin"""
+
+"""mit 
+durchgang = 0
+aktiv = True
+while aktiv:
+    print(durchgang)
+    durchgang = durchgang + 1
+    if (durchgang == 3):
+        aktiv = False
+print("nach der Schleife")"""
