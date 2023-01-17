025ee9e277dc4967d9b5a7aab458aaf4a374e745
sophiia220403 <stoesss@dhbw-loerrach.de>
2023-01-17 15:12:11 +0100

    Aufgabe für Daheim

--- whileSchleife.py
+++ whileSchleife.py
@@ -13,3 +13,47 @@ while aktiv:
     if (durchgang == 3):
         aktiv = False
 print("nach der Schleife")"""
+
+def epochen():
+    Jahr = int(input("Bitte Jahr eingeben \n"))
+
+    if weitermachen == "weiter":
+        print("ok, weiter geht es!")
+        epochen()
+    else:
+        print("ok, tschüss")
+
+    if Jahr < 0:
+        print("vor Christus")
+
+    elif 0 < Jahr <= 500:
+        print("mit Christus")
+
+    elif 500 < Jahr <= 750:
+        print("frühes Mittelalter")
+
+    elif 750 < Jahr <= 1250:
+        print("hohes Mittelalter")
+
+    elif 1250 < Jahr <= 1500:
+        print("spätes Mittelalter")
+
+    elif 1500 < Jahr <= 1600:
+        print("Neuzeit")
+
+    else:
+        print("Bitte eine Zahl zwischen 0 und 1600 eingeben!")
+
+durchgang = 0
+aktiv = True
+while aktiv:
+    #print(durchgang)
+    durchgang = durchgang + 1
+    weitermachen = input("soll ich weitermachen? Ja/Nein \n")
+    if weitermachen != "Ja":
+        aktiv = False
+    else:
+        print("ok, tschüss")
+#print("nach der Schleife")
+
+#Zahlenratespiel
